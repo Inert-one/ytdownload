c0372240a9b06683e825c62ea7a735907591c01b
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2020-07-22 12:03:12 +0200

    1.1
    Added English Language. Fixed bugs.

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ while True:
     if event == 'MP4':
         print('Downloading..')
         YouTube(values['link']).streams.first().download()
-        sg.popup('Pobrane.', title='Plik MP4 został pobrany.')
+        sg.popup('Success.', title='MP4 File was downloaded.')
     if event == 'MP3':
         print('Downloading..')
         input = ffmpeg.input(YouTube(values['link']).streams.first().download())
         audio = input.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
         video = input.video.hflip()
         out = ffmpeg.output(audio, 'convert.mp3')
-        sg.popup('Pobrane.', title='Plik MP3 został pobrany.')
+        sg.popup('Success.', title='MP3 file was downloaded.')
 
 
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
